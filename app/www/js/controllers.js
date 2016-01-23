c95f1b86e111b385101ec58ebaafc669da9b3a8d
Kyle Permuy <kyle@varninja.com>
2016-01-23 18:43:22 -0500

    Add more app controller logic

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -4,6 +4,8 @@ angular.module('halo.controllers', [])
 
     $scope.total = 100;
     $scope.temp = $scope.gas = $scope.rain = 0;
+    $scope.progressGas = $scope.progressRain = "None";
+    $scope.rainColor = $scope.gasColor = "#00FF00";
 
     function parseSensorData() {
         Dash.fetchSensorInfo().then(function(data){
@@ -11,12 +13,28 @@ angular.module('halo.controllers', [])
             $scope.gas = data.Item.currentGas;
             $scope.rain = data.Item.currentRain;
             $scope.progress = Math.round(data.Item.currentTemp);
-            $scope.progressGas = "None";
-            $scope.progressRain = "None";
-        })
+        }).then(function(data){
+            if ($scope.gas < 40){
+                $scope.progressGas = "Some";
+                $scope.gasColor = "#FF0000";
+            }
+
+            if ($scope.rain < 150){
+                $scope.progressRain = "Some";
+                $scope.rainColor = "#FF0000";
+            }
+        });
     }
 
     parseSensorData();
+
+    for (var i = 0; i < 100; i++) {
+        (function (i) {
+            setTimeout(function () {
+                parseSensorData();
+            }, 3000)
+        })(i)
+    }
 
     $scope.rain = 100;
 
