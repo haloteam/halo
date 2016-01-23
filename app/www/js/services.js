0cf5f8825bc24c2a1c5102b95bd640162490f51d
Kyle Permuy <kyle@varninja.com>
2016-01-23 01:37:53 -0500

    Working on Controllers and Services

--- app/www/js/services.js
+++ app/www/js/services.js
@@ -1,16 +1,9 @@
 angular.module('halo.services', [])
 
-.factory('Chats', function() {
-
-  return {
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
-  };
-  
+.factory('Dash', function($http) {
+   return {
+     fetchSensorInfo: function(callback) {
+       $http.get('https://a9a0t0l599.execute-api.us-east-1.amazonaws.com/prod/Halo').success(callback);
+     }
+   }
 });
