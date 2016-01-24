66b64ac3297c16390df81fc9cbc3fe080f85a038
Me <me@Mes-MacBook-Pro.local>
2016-01-24 03:21:25 -0500

    conversation starters

--- raspi/Halo.py
+++ raspi/Halo.py
@@ -50,11 +50,6 @@ class Halo:
         GPIO.setup(self.GAS_SENSOR_PIN, GPIO.IN)
         GPIO.setup(self.BUZZ_PIN, GPIO.OUT)
         GPIO.setup(self.H2O_PIN, GPIO.IN)
-        try:
-            wit.init()
-            speech_response = wit.voice_query_auto(self.wit_access_token)
-        except Exception as err:
-            print err
 
         # setup pins for "eyes"
         # first eye
