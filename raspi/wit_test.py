9b9ad1a2ca3d46946559739857e8061f37844685
Me <me@Mes-MacBook-Pro.local>
2016-01-24 00:26:01 -0500

    wit_test

--- raspi/wit_test.py
+++ raspi/wit_test.py
@@ -1,5 +1,4 @@
 import wit
 wit.init()
 wit_access_token = '5HO7GQT6GHYYBC4G2M5SPTCWXSNSEL4S'
-speech_response = wit.voice_query_auto(wit_access_token)
-print speech_response
+wit.voice_query_auto(wit_access_token)
